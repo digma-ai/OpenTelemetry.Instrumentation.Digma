048340462e667cc5f4e81e15bda241fd0b83f2bf
asafchen-dig <achen@digma.ai>
2025-04-16 12:34:49 -0400

    ConsoleAppDotNet

--- src/Tests/AutoInstrumentation.ZeroCodeTests/WindowsServiceTests.cs
+++ src/Tests/AutoInstrumentation.ZeroCodeTests/WindowsServiceTests.cs
@@ -1,12 +1,8 @@
-﻿using System.Diagnostics;
-using AutoInstrumentation.ZeroCodeTests.OsServices;
+﻿using AutoInstrumentation.ZeroCodeTests.OsServices;
 using AutoInstrumentation.ZeroCodeTests.OtelCollector;
 using AutoInstrumentation.ZeroCodeTests.Utils;
 using FluentAssertions;
-using FluentAssertions.Extensions;
-using Grpc.Net.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenTelemetry.Proto.Collector.Trace.V1;
 
 
 namespace AutoInstrumentation.ZeroCodeTests;
@@ -51,6 +47,62 @@ public class WindowsServiceTests
             span.Attributes.Should().ContainSingle(x =>
                 x.Key == "code.namespace" &&
                 x.Value.StringValue == "AutoInstrumentation.ConsoleAppNetFramework.UsersRepository");
+            
+            span.Attributes.Should().ContainSingle(x =>
+                x.Key == "code.function" &&
+                x.Value.StringValue == "GetAllUsers");
+        });
+    }
+          
+    [TestMethod]
+    public void Sanity_ConsoleApp_DotNet8()
+    {
+        var exeFilePath = Path.GetFullPath(@"..\..\..\..\AutoInstrumentation.ConsoleAppDotNet\bin\Debug\net8.0\AutoInstrumentation.ConsoleAppDotNet.exe");
+        _windowsServiceManager.CreateService(_serviceName, exeFilePath, 0);
+        _windowsServiceManager.InstrumentService(_serviceName, 
+            $"http://localhost:{OtelCollectorInitializer.Port}/v1/traces/" , 
+            "http/protobuf");
+
+        Retry.Do(() =>
+        {
+            var spans = OtelCollectorInitializer.GetSpans(_serviceName);
+
+            var span = spans.FirstOrDefault(x => x.Scope.Name == "UsersRepository" && x.Span.Name == "GetAllUsers")
+                ?.Span;
+
+            span.Should().NotBeNull();
+            
+            span.Attributes.Should().ContainSingle(x =>
+                x.Key == "code.namespace" &&
+                x.Value.StringValue == "AutoInstrumentation.ConsoleAppDotNet.UsersRepository");
+            
+            span.Attributes.Should().ContainSingle(x =>
+                x.Key == "code.function" &&
+                x.Value.StringValue == "GetAllUsers");
+        });
+    }
+                 
+    [TestMethod]
+    public void Sanity_ConsoleApp_DotNet9()
+    {
+        var exeFilePath = Path.GetFullPath(@"..\..\..\..\AutoInstrumentation.ConsoleAppDotNet\bin\Debug\net9.0\AutoInstrumentation.ConsoleAppDotNet.exe");
+        _windowsServiceManager.CreateService(_serviceName, exeFilePath, 0);
+        _windowsServiceManager.InstrumentService(_serviceName, 
+            $"http://localhost:{OtelCollectorInitializer.Port}/v1/traces/" , 
+            "http/protobuf");
+
+        Retry.Do(() =>
+        {
+            var spans = OtelCollectorInitializer.GetSpans(_serviceName);
+
+            var span = spans.FirstOrDefault(x => x.Scope.Name == "UsersRepository" && x.Span.Name == "GetAllUsers")
+                ?.Span;
+
+            span.Should().NotBeNull();
+            
+            span.Attributes.Should().ContainSingle(x =>
+                x.Key == "code.namespace" &&
+                x.Value.StringValue == "AutoInstrumentation.ConsoleAppDotNet.UsersRepository");
             
             span.Attributes.Should().ContainSingle(x =>
                 x.Key == "code.function" &&
